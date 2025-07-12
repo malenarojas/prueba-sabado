d07e2cdc5329b512760002388ccb849147dff25c
malenarojas <malenarojascamargo@@hotmail.com>
2025-07-12 16:35:28 -0400

    Correct typo in header text in App component

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
 
-        <h1>Esta prueba es del sabado</h1>
+        <h1>Esta prueba es del sabado correcto </h1>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
